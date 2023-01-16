ca8c68086e158aa85902ad4a57c876b544cee2a6
Olivier F <94624011+OlivierFacelina@users.noreply.github.com>
2023-01-16 10:10:45 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,26 @@ for (let i = 0; i < buttons.length; i++) {
   Résultat : ${resultat}
 `  ;
     
+    if(typeof localStorage!='undefined') {
+  // Récupération de la valeur dans web storage
+  var nbvisites = localStorage.getItem('visites');
+  // Vérification de la présence du compteur
+  if(nbvisites!=null) {
+    // Si oui, on convertit en nombre entier la chaîne de texte qui fut stockée
+    nbvisites = parseInt(nbvisites);
+  } else {
+    nbvisites = 1;
+  }
+  // Incrémentation
+  nbvisites++;
+  // Stockage à nouveau en attendant la prochaine visite...
+  localStorage.setItem('visites',nbvisites);
+  // Affichage dans la page
+  document.getElementById('visites').innerHTML = nbvisites;
+} else {
+  alert("localStorage n'est pas supporté");
+}
+    
   });
   
   
